263e0557aa25e034891a426df8a8d87ac613b020
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2023-05-28 15:43:45 -1000

    Update next.config.js

--- application/next.config.js
+++ application/next.config.js
@@ -1,5 +1,8 @@
 
 module.exports = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
